e1920072bc4e8ae5fb4dce4a98db357a25ede564
yundeveloper <yuniorbatista1113@gmail.com>
2023-09-01 19:57:04 -0400

    removed console.log

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   const [businesses, setBusinesses] = useState([]);
 
   const handleBusinessChange = (updatedBusinesses) => {
-    console.log("updated businesses in app", updatedBusinesses);
     setBusinesses(updatedBusinesses);
   };
   return (
